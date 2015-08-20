e60a392cec6c7d73a108f47988245846c5d0a3ec
Esteban Parra <estebanpr014@gmail.com>
2015-08-20 18:05:41 -0400

    Project Template Created
    Created template for Hardwood Seller program:
    - WoodItem class defined with constructiors and getter methods. Blank
    toString method
    - Main class with empty methods for delivery time computation and input
    file reading

---
+++ src/controller/HardwoodSeller.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author Esteban
+ *
+ */
+public class HardwoodSeller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void readInputFile(String inputFilePath){
+		
+	}
+	
+	public Double deliveryTime(){
+		Double deliveryETA = 0.0;
+		return deliveryETA;
+	}
+	
+}
---
+++ src/model/WoodItem.java
@@ -0,0 +1,74 @@
+/**
+ * Class defining the elements of a wood type
+ */
+package model;
+
+/**
+ * @author USER
+ *
+ */
+public class WoodItem {
+
+	String type;
+	Double daseDeliveryTime;
+	Double price;
+	
+	/**
+	 * Default constructor 
+	 */
+	public WoodItem(){
+		this.type = "Empty";
+		this.daseDeliveryTime = 0.0;
+		this.price = 0.0;
+	}
+	
+	/**
+	 * Constructor of a WoodItem element with parameters
+	 * @param type Name of the wood type
+	 * @param daseDeliveryTime base delivery time
+	 * @param price price per Bare Foot
+	 */
+	public WoodItem(String type, Double daseDeliveryTime, Double price) {
+		this.type = type;
+		this.daseDeliveryTime = daseDeliveryTime;
+		this.price = price;
+	}
+
+	/**
+	 * Get the type of Wood
+	 * 
+	 * @return Name of the WoodItem
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * Get the base delivery time
+	 * 
+	 * @return the baseDeliverytime
+	 */
+	public Double getDaseDeliveryTime() {
+		return daseDeliveryTime;
+	}
+
+	/**
+	 * 
+	 * Get the price per Bare Foot
+	 * 
+	 * @return the price for 1 Bare Foot of this WoodItem
+	 */
+	public Double getPrice() {
+		return price;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 * You may implement this method to fit your needs
+	 */
+	@Override
+	public String toString() {	
+		return "";
+	}
+	
+}
