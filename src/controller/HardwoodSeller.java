b0113ad06ea8d7052cb5262ca161f65b1590ed90
jciacco15 <jmc11k@my.fsu.edu>
2016-01-26 20:29:59 -0500

    Final HardwoodSeller.java
    completed java program for HardwoodSeller.java

--- src/controller/HardwoodSeller.java
+++ src/controller/HardwoodSeller.java
@@ -16,7 +16,7 @@ public class HardwoodSeller {
 			Scanner lineScanner = new Scanner(new File(args[0]));
 			//read line containing name, address and date
 			String firstLine = lineScanner.nextLine();
-			System.out.println(firstLine);
+			
 			
 			//read line containing wood types and bf
 			
@@ -30,7 +30,7 @@ public class HardwoodSeller {
 			
 			//add wood names and BF to respective ArrayList
 			StringTokenizer s = new StringTokenizer(woodOrder);
-			System.out.println(s.countTokens());
+			//System.out.println(s.countTokens());
 			while(s.hasMoreTokens()){
 				String word = s.nextToken();
 				if((word.equals("White"))){
@@ -57,18 +57,6 @@ public class HardwoodSeller {
 				}
 			}
 			
-			//print out wood type for testing
-			for(int i=0; i < woodType.size(); i++){
-				System.out.println(woodType.get(i));
-				if(woodType.get(i).equals("Cherry")){
-					System.out.println("yes");
-				}
-			}
-			
-			//print out BF for testing
-			for(int j=0; j < bF.size(); j++){
-				System.out.println(bF.get(j));
-			}
 			
 			calculate(woodType, bF, firstLine);
 		}
@@ -81,29 +69,175 @@ public class HardwoodSeller {
 		
 		double price = 0;
 		double deliveryTime = 0;
+		double cherryPrice = 0;
+		double wengePrice = 0;
+		double whiteOakPrice = 0;
+		double sawdustPrice = 0; 
+		double maplePrice = 0;
+		double mahoganyPrice =0;
+		
+		//calculate price and delivery time
 		for(int i=0; i < wood.size(); i++){
 			if(wood.get(i).equals("Cherry")){
 				price += boardFoot.get(i) * 5.95;
+				cherryPrice = boardFoot.get(i) * 5.95;
+				if(boardFoot.get(i) >=1 && boardFoot.get(i) <=100){
+					deliveryTime += 2.5*1;
+				}
+				else if(boardFoot.get(i) >=101 && boardFoot.get(i) <=200){
+					deliveryTime += 2.5*2;
+				}
+				else if(boardFoot.get(i) >=201 && boardFoot.get(i) <=300){
+					deliveryTime += 2.5*3;
+				}
+				else if(boardFoot.get(i) >=301 && boardFoot.get(i) <=400){
+					deliveryTime += 2.5*4;
+				}
+				else if(boardFoot.get(i) >=401 && boardFoot.get(i) <=500){
+					deliveryTime += 2.5*5;
+				}
+				else if(boardFoot.get(i) >=501 && boardFoot.get(i) <=1000){
+					deliveryTime += 2.5*5.5;
+				}
 			}
 			else if(wood.get(i).equals("Wenge")){
 				price += boardFoot.get(i) * 22.35;
+				wengePrice = boardFoot.get(i) * 22.35;
+				if(boardFoot.get(i) >=1 && boardFoot.get(i) <=100){
+					deliveryTime += 5*1;
+				}
+				else if(boardFoot.get(i) >=101 && boardFoot.get(i) <=200){
+					deliveryTime += 5*2;
+				}
+				else if(boardFoot.get(i) >=201 && boardFoot.get(i) <=300){
+					deliveryTime += 5*3;
+				}
+				else if(boardFoot.get(i) >=301 && boardFoot.get(i) <=400){
+					deliveryTime += 5*4;
+				}
+				else if(boardFoot.get(i) >=401 && boardFoot.get(i) <=500){
+					deliveryTime += 5*5;
+				}
+				else if(boardFoot.get(i) >=501 && boardFoot.get(i) <=1000){
+					deliveryTime += 5*5.5;
+				}
 			}
 			else if(wood.get(i).equals("Sawdust")){
 				price += boardFoot.get(i) * 1.5;
+				sawdustPrice = boardFoot.get(i) * 1.5;
+				if(boardFoot.get(i) >=1 && boardFoot.get(i) <=100){
+					deliveryTime += 1;
+				}
+				else if(boardFoot.get(i) >=101 && boardFoot.get(i) <=200){
+					deliveryTime += 2;
+				}
+				else if(boardFoot.get(i) >=201 && boardFoot.get(i) <=300){
+					deliveryTime += 3;
+				}
+				else if(boardFoot.get(i) >=301 && boardFoot.get(i) <=400){
+					deliveryTime += 4;
+				}
+				else if(boardFoot.get(i) >=401 && boardFoot.get(i) <=500){
+					deliveryTime += 5;
+				}
+				else if(boardFoot.get(i) >=501 && boardFoot.get(i) <=1000){
+					deliveryTime += 5.5;
+				}
 			}
 			else if(wood.get(i).equals("White Oak")){
 				price += boardFoot.get(i) * 6.70;
+				whiteOakPrice = boardFoot.get(i) * 6.70;
+				if(boardFoot.get(i) >=1 && boardFoot.get(i) <=100){
+					deliveryTime += 2.3*1;
+				}
+				else if(boardFoot.get(i) >=101 && boardFoot.get(i) <=200){
+					deliveryTime += 2.3*2;
+				}
+				else if(boardFoot.get(i) >=201 && boardFoot.get(i) <=300){
+					deliveryTime += 2.3*3;
+				}
+				else if(boardFoot.get(i) >=301 && boardFoot.get(i) <=400){
+					deliveryTime += 2.3*4;
+				}
+				else if(boardFoot.get(i) >=401 && boardFoot.get(i) <=500){
+					deliveryTime += 2.3*5;
+				}
+				else if(boardFoot.get(i) >=501 && boardFoot.get(i) <=1000){
+					deliveryTime += 2.3*5.5;
+				}
 			}
 			else if(wood.get(i).equals("Curly Maple")){
 				price += boardFoot.get(i) * 6.00;
+				maplePrice = boardFoot.get(i) * 6.00;
+				if(boardFoot.get(i) >=1 && boardFoot.get(i) <=100){
+					deliveryTime += 1.5*1;
+				}
+				else if(boardFoot.get(i) >=101 && boardFoot.get(i) <=200){
+					deliveryTime += 1.5*2;
+				}
+				else if(boardFoot.get(i) >=201 && boardFoot.get(i) <=300){
+					deliveryTime += 1.5*3;
+				}
+				else if(boardFoot.get(i) >=301 && boardFoot.get(i) <=400){
+					deliveryTime += 1.5*4;
+				}
+				else if(boardFoot.get(i) >=401 && boardFoot.get(i) <=500){
+					deliveryTime += 1.5*5;
+				}
+				else if(boardFoot.get(i) >=501 && boardFoot.get(i) <=1000){
+					deliveryTime += 1.5*5.5;
+				}
 			}
 			else if(wood.get(i).equals("Genuine Mahogany")){
 				price += boardFoot.get(i) * 9.60;
+				mahoganyPrice = boardFoot.get(i) * 9.60;
+				if(boardFoot.get(i) >=1 && boardFoot.get(i) <=100){
+					deliveryTime += 3*1;
+				}
+				else if(boardFoot.get(i) >=101 && boardFoot.get(i) <=200){
+					deliveryTime += 3*2;
+				}
+				else if(boardFoot.get(i) >=201 && boardFoot.get(i) <=300){
+					deliveryTime += 3*3;
+				}
+				else if(boardFoot.get(i) >=301 && boardFoot.get(i) <=400){
+					deliveryTime += 3*4;
+				}
+				else if(boardFoot.get(i) >=401 && boardFoot.get(i) <=500){
+					deliveryTime += 3*5;
+				}
+				else if(boardFoot.get(i) >=501 && boardFoot.get(i) <=1000){
+					deliveryTime += 3*5.5;
+				}
 			}
 		}
 		
-		System.out.println("Price: " + price);
-		
+		//print out entire wood order
+		System.out.println("Wood Order:");
+		System.out.println(name);
+		for(int i=0; i < wood.size(); i++){
+			System.out.print(wood.get(i) + ", BF: " + boardFoot.get(i));
+			if(wood.get(i).equals("Cherry")){
+				System.out.println(", Price: $" + cherryPrice);
+			}
+			else if(wood.get(i).equals("Wenge")){
+				System.out.println(", Price: $" + wengePrice);
+			}
+			else if(wood.get(i).equals("White Oak")){
+				System.out.println(", Price: $" + whiteOakPrice);
+			}
+			else if(wood.get(i).equals("Curly Maple")){
+				System.out.println(", Price: $" + maplePrice);
+			}
+			else if(wood.get(i).equals("Genuine Mahogany")){
+				System.out.println(", Price: $" + mahoganyPrice);
+			}
+			else if(wood.get(i).equals("Sawdust")){
+				System.out.println(", Price: $" + sawdustPrice);
+			}
+		}
+		System.out.println("Total Price: $" + price);
+		System.out.println("Delivery Time: " + deliveryTime + " hours");
 	}
 	
 }
